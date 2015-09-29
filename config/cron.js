0deafae79a2c1c2082b9fde67cfb212fe5e5e1fd
Ryan J. Froehlich <ryan@synergy-cns.com>
2015-09-29 17:19:44 -0500

    Testing cron

--- config/cron.js
+++ config/cron.js
@@ -3,5 +3,23 @@
  *
  * Look here for detailed examples https://github.com/ghaiklor/sails-hook-cron
  */
+var AWS = require('aws-sdk');
+var Lambda = new AWS.Lambda({ region: 'us-east-1' });
 
-module.exports.cron = {};
+module.exports.cron = {
+  cleanupPublicFTP: {
+    schedule: '25 18 * * *',
+    onTick: function() {
+      sails.log.info('Cleaning public FTP server...');
+      Lambda.invoke({ FunctionName: "fnCleanupPublicFTP" }, function(err, data) {
+        if(err) {
+          sails.log.error('An error occurred while trying to cleanup the public FTP server!', err);
+        } else {
+          sails.log.success('FTP server cleaned!', data);
+        }
+      });
+    },
+    timezone: 'America/New_York',
+    start: true
+  }
+};
