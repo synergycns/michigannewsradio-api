b76cd4332685ec07e006bdb709bb0acb3c7858a7
Ryan J. Froehlich <ryan@synergy-cns.com>
2015-09-29 19:50:14 -0500

    Add new exec cron job

--- config/cron.js
+++ config/cron.js
@@ -3,19 +3,19 @@
  *
  * Look here for detailed examples https://github.com/ghaiklor/sails-hook-cron
  */
-var AWS = require('aws-sdk');
-var Lambda = new AWS.Lambda({ region: 'us-east-1' });
+var exec = require('child_process').exec;
 
 module.exports.cron = {
   cleanupPublicFTP: {
-    schedule: '40 18 * * *',
+    schedule: '55 20 * * *',
     onTick: function() {
       sails.log.info('Cleaning public FTP server...');
-      Lambda.invoke({ FunctionName: "fnCleanupPublicFTP" }, function(err, data) {
-        if(err) {
-          sails.log.error('An error occurred while trying to cleanup the public FTP server!', err);
+      var sCommand = "aws lambda invoke --function-name fnCleanupPublicFTP --region us-east-1 /tmp/output.txt";
+      exec(sCommand, function(error) {
+        if(error) {
+          sails.log.error('Error cleaning public FTP server!', error);
         } else {
-          sails.log.success('FTP server cleaned!', data);
+          sails.log('Public FTP server cleaned successfully!');
         }
       });
     },
